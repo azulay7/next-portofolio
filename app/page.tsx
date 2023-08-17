21dd4563f345f505b660a8a9bd98f6ed7928e8dc
Amit Azulay <azulay7@gmail.com>
2023-08-17 17:23:32 +0300

    blank link

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
                 <BsFillMoonStarsFill className="cursor-pointer" />
               </li>
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="/public/resume.pdf" >
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#" >
                   Resume
                 </a>
               </li>
